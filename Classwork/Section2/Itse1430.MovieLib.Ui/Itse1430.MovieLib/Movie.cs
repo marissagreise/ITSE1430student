c954526aa14ab040fc1b0584117fae10a6d1339f
marissagreise <marissa.greise@my.tccd.edu>
2018-09-17 19:12:16 -0500

    Section2

---
+++ Classwork/Section2/Itse1430.MovieLib.Ui/Itse1430.MovieLib/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Itse1430.MovieLib
+{
+    class Movie
+    {
+        public string Name;
+        // public System.String Name; fully qualified namespace
+
+
+    }
+}
