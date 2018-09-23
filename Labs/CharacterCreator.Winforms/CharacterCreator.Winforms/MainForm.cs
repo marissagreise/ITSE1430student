e6275feff00c9dd2b6b1a20ec6c71229e12ad2a6
Marissa Greise <marissa.greise@my.tccd.edu>
2018-09-23 17:02:34 -0500

    About MEssage

---
+++ Labs/CharacterCreator.Winforms/CharacterCreator.Winforms/MainForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CharacterCreator.Winforms
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click( object sender, EventArgs e )
+        {
+            if (MessageBox.Show("Are you sure you want to exit?",
+                       "Close", MessageBoxButtons.YesNo) == DialogResult.No)
+                return;
+
+            Close();
+        }
+
+        private void aboutToolStripMenuItem_Click( object sender, EventArgs e )
+        {
+            MessageBox.Show(this, "Sorry", "Help", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+    }
+}
