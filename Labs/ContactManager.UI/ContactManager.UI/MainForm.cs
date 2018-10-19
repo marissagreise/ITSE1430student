e7a51a47e740df7b7160e3521407daccb5e319dc
Marissa Greise <marissa.greise@my.tccd.edu>
2018-10-19 11:28:51 -0500

    feed1

---
+++ Labs/ContactManager.UI/ContactManager.UI/MainForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ContactManager.UI
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void OnFileExit( object sender, EventArgs e )
+        {
+            if (MessageBox.Show("Are you sure you want to exit?",
+                       "Close", MessageBoxButtons.YesNo) == DialogResult.No)
+                return;
+
+            Close();
+        }
+
+        private void OnHelpAbout( object sender, EventArgs e )
+        {
+            MessageBox.Show(this, "Marissa Greise\nITSE 1430\nContact Manager",
+               "Help", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+    }
+}
