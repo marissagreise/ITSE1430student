fc171a3620be05a3b742e1e7eb45db49cc864f30
marissagreise <marissa.greise@my.tccd.edu>
2018-11-12 18:55:22 -0600

    money

---
+++ Classwork/Section2/Itse1430.MovieLib.Ui/Itse1430.MovieLib.Sql/SqlMovie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Itse1430.MovieLib.Sql
+{
+    internal class SqlMovie : Movie //priv
+    {
+        public int Id { get; set; }
+    }
+}
--- Classwork/Section2/Itse1430.MovieLib.Ui/Itse1430.MovieLib.Sql/SqlMovieDatabase.cs
+++ Classwork/Section2/Itse1430.MovieLib.Ui/Itse1430.MovieLib.Sql/SqlMovieDatabase.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -76,19 +77,82 @@ namespace Itse1430.MovieLib.Sql
             };
         }
 
-        private object GetMovieId( Movie oldMovie )
+        private object GetMovieId( Movie movie )
         {
-            return 1;
+            var sql = movie as SqlMovie;
+
+            return sql?.Id ?? 0;
         }
 
         protected override Movie FindByName( string name )
         {
-            throw new NotImplementedException();
+            // us a data reader
+            using (var conn = CreateConnection())
+            {
+                var cmd = new SqlCommand("GetAllMovies", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                conn.Open();
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var movieName = reader.GetString(1);
+                        if (String.Compare(movieName, name, true) != 0)
+                            continue;
+
+                        return new SqlMovie()
+                        {
+
+                            Id = reader.GetFieldValue<int>(0),
+                            Name = movieName,
+                            Description = Convert.ToString(reader.GetValue(2)),
+                            ReleaseYear = 1900,
+                            RunLength = reader.GetFieldValue<int>(3),
+                            IsOwned = reader.GetBoolean(4),
+                        };
+                    };
+                };
+            };
+
+            return null;
         }
 
         protected override IEnumerable<Movie> GetAllCore()
         {
-            return new Movie[0];
+            var ds = new DataSet();
+
+            using (var conn = CreateConnection())
+            {
+                var da = new SqlDataAdapter();
+                var cmd = new SqlCommand("GetAllMovies", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                da.SelectCommand = cmd;
+                da.Fill(ds);
+            };
+
+            //Read data
+            //if (!ds.Tables.OfType<DataTable>().Any()) //compa. extension. and impl. IEmum<T>
+       
+            var table = ds.Tables.OfType<DataTable>().FirstOrDefault();
+            if (table == null)
+                return Enumerable.Empty<Movie>();
+
+            var movies = new List<Movie>();
+            foreach (var row in table.Rows.OfType<DataRow>())
+            {
+                var movie = new SqlMovie() {
+                    Id = Convert.ToInt32(row["Id"]),
+                    Name = row.Field<string>("Title"),
+                    Description = Convert.ToString(row[2]),
+                    ReleaseYear = 1900,
+                    RunLength = row.Field<int> (3),
+                    IsOwned = Convert.ToBoolean(row[4]),
+                };
+                movies.Add(movie);
+            };
+            return movies;
         }
 
         protected override void RemoveCore( string name )
