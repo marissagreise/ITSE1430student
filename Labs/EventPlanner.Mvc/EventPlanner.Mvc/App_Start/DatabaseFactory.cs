6310663e40d7515ee3be20d9e548d46cb90b0902
Marissa Greise <marissa.greise@my.tccd.edu>
2018-12-05 21:51:39 -0600

    2ish

---
+++ Labs/EventPlanner.Mvc/EventPlanner.Mvc/App_Start/DatabaseFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EventPlanner.Mvc.App_Start
+{
+    public static class DatabaseFactory
+    {
+        private static IEventDatabase EventDatabase;
+    }
+}
---
+++ Labs/EventPlanner.Mvc/EventPlanner.Mvc/Models/EventModel.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EventPlanner.Mvc.Models
+{
+    public class EventModel
+    {
+            public EventModel()
+            {
+            }
+
+            //Using simple mapping layer using a constructor
+            public EventModel( EventPlanner.ScheduledEvent item )
+            {
+                if (item != null)
+                {
+                    Id = item.Id;
+                    Name = item.Name;
+                    Description = item.Description;
+                    StartDate = item.StartDate;
+                    EndDate = item.EndDate;
+                  
+                };
+            }
+
+            ////Using a simple mapping layer using a method
+            //public Itse1430.MovieLib.Movie ToDomain()
+            //{
+            //    return new Itse1430.MovieLib.Movie()
+            //    {
+            //        Name = Name,
+            //        Description = Description,
+            //        ReleaseYear = ReleaseYear,
+            //        RunLength = RunLength,
+            //        IsOwned = IsOwned,
+            //    };
+            //}
+            
+
+            [Required(AllowEmptyStrings = false)]
+            public string Name { get; set; }
+
+            public string Description { get; set; }
+
+            [Display(Name = "Start Date")]
+            public DateTime StartDate { get; set; };
+
+            [Display(Name = "End Date")]
+            [DateCorrectRange(ValidateEndDate = true, ErrorMessage = "End Date must be >= Start Date.")]
+            public DateTime EndDate { get; set; }
+
+            public int Id { get; set; }
+
+        }
+        [AttributeUsage(AttributeTargets.Property)]
+        public class DateCorrectRangeAttribute : ValidationAttribute
+        {
+            public bool ValidateStartDate { get; set; }
+            public bool ValidateEndDate { get; set; }
+
+            protected override ValidationResult IsValid( object value, ValidationContext validationContext )
+            {
+                var model = validationContext.ObjectInstance as EventModel;
+
+                if (model != null)
+                {
+                    if (model.StartDate > model.EndDate && ValidateEndDate
+                        || (model.StartDate > DateTime.Now.Date && ValidateStartDate))
+                    {
+                        return new ValidationResult(string.Empty);
+                    }
+                }
+
+                return ValidationResult.Success;
+            }
+        }
+    }
